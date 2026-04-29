e144ac3a1e402543adbf38ea9a0d6484ba22e51f
Mercy Christina Lanka <lmercychristina@gmail.com>
2026-04-29 19:29:00 -0400

    Fix all module file extensions

--- Env/dev/backend.tf.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "jenkins_rg"
-    storage_account_name = "devbackendrg"
-    container_name       = "mybackup"
-    key                  = "dev.tfstate"
-  }
-}
--- Env/dev/var.tf.tf
+++
@@ -1,8 +0,0 @@
-variable "mod_rg_name" {
-  type = string
-}
-
-variable "mod_rg_loc" {
-  type = string
-}
-
